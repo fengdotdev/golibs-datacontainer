98de8c790e4a57f474fb05332d2e9dd777f242ad
feng.dev <feng@vitruvio.cl>
2025-06-18 18:40:26 -0400

    Update main.go

--- cmd/playground/main.go
+++ cmd/playground/main.go
@@ -3,70 +3,28 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
 	"sync"
-	"time"
 
-	"github.com/fengdotdev/golibs-datacontainer/sandbox/draft1/concurrent/golimiter"
 	"github.com/fengdotdev/golibs-datacontainer/sandbox/draft1/concurrent/gomap"
 )
 
 func main() {
+	defer recoverPanic()
 	GoMapAndLimiter()
 }
 
 func GoMapAndLimiter() {
+	defer recoverPanic()
 
-	mmyMap := gomap.NewGoMap[string, int]()
-	defer mmyMap.Save()
-	mmyMap.SetSaver(
-		func(data []byte) error {
-			// save on disk
+	mmyMap := gomap.NewGoMapWithBuffer[string, int](20)
 
-			path := "data.gob"
-			file, err := os.Create(path)
-			if err != nil {
-				return fmt.Errorf("failed to create file: %w", err)
-			}
-			defer file.Close()
-			_, err = file.Write(data)
-			if err != nil {
-				return fmt.Errorf("failed to write data to file: %w", err)
-			}
-			fmt.Println("Data saved to", path)
-			return nil
-		},
-	)
 	var wg sync.WaitGroup
-	ltr := golimiter.New(100) // Create a new limiter with a limit of 100 concurrent jobs
-	running := bool(true)
-	go func() {
 
-		for running {
-			time.Sleep(5 * time.Second) //
-			fmt.Println("Current limit:", ltr.CurrentLimit())
-			fmt.Println("Is limiter full?", ltr.IsFull())
-		}
-
-	}()
-
-	go func() {
-		for running {
-			time.Sleep(3 * time.Minute) //
-			err := mmyMap.Save()
-			if err != nil {
-				panic(fmt.Sprintf("Error saving map: %v", err))
-			}
-		}
-	}()
-
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 1000000; i++ {
 
 		wg.Add(1)
-		ltr.Acquire() // Acquire a slot in the limiter
 		go func(i int) {
-			defer ltr.Release() // Ensure the slot is released when done
-
+			defer recoverPanic()
 			defer wg.Done()
 			err := <-mmyMap.Set(context.Background(), fmt.Sprintf("key%d", i), i)
 			if err != nil {
@@ -74,6 +32,7 @@ func GoMapAndLimiter() {
 			} else {
 				//fmt.Println("Goroutine", i, "overwrote value for key:", fmt.Sprintf("key%d", i))
 			}
+			fmt.Println("Goroutine", i, "set value for key:", fmt.Sprintf("key%d", i))
 
 		}(i)
 	}
@@ -84,12 +43,11 @@ func GoMapAndLimiter() {
 		fmt.Println("Error setting value:", err)
 	}
 
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 10000000; i++ {
 
 		wg.Add(1)
-		ltr.Acquire() // Acquire a slot in the limiter
 		go func(i int) {
-			defer ltr.Release() // Ensure the slot is released when done
+			defer recoverPanic()
 			defer wg.Done()
 
 			valueChan, errChan := mmyMap.Get(context.Background(), "key1")
@@ -98,18 +56,13 @@ func GoMapAndLimiter() {
 			} else {
 				<-valueChan
 			}
+			fmt.Println("Goroutine", i, "retrieved")
 
 		}(i)
 	}
 
 	wg.Wait()
-	running = false // Stop the goroutine that prints the current limit
 	fmt.Println("All goroutines completed.")
-	err = mmyMap.Save()
-	if err != nil {
-		panic(fmt.Sprintf("Error saving map: %v", err))
-	}
-	fmt.Println("Map saved successfully.")
 
 	lenchan, errchan := mmyMap.Size(context.Background())
 	if err := <-errchan; err != nil {
@@ -117,5 +70,12 @@ func GoMapAndLimiter() {
 		return
 	}
 
-	<-lenchan
+	len := <-lenchan
+	fmt.Println("Size of the map:", len)
+}
+
+func recoverPanic() {
+	if r := recover(); r != nil {
+		fmt.Println("Recovered from panic:", r)
+	}
 }
